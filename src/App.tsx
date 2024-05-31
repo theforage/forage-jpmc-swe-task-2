00b98c255c6e79166b6a318c82b1de456b1d8d14
kspn675 <116645336+kspn675@users.noreply.github.com>
2024-05-31 22:30:43 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean, // Add this line to manage the graph's visibility
 }
 
 /**
@@ -22,6 +23,7 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph: false, // Initialize showGraph to false
     };
   }
 
@@ -29,18 +31,36 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    if (this.state.showGraph) { // Conditionally render the graph
+      return (<Graph data={this.state.data}/>);
+    }
+    return null;
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+    // Set showGraph to true when starting to fetch data
+    this.setState({ showGraph: true });
+    // Continuously fetch data every 100ms
+    const intervalId = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        // Update the state by creating a new array of data that consists of
+        // Previous data in the state and the new data from server
+        this.setState({ data: [...this.state.data, ...serverResponds] });
+      });
+    }, 100);
+
+    // Store intervalId to clear it later if needed
+    this.intervalId = intervalId;
+  }
+
+  // Optional: Add componentWillUnmount to clear the interval when the component unmounts
+  componentWillUnmount() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
   }
 
   /**
@@ -56,9 +76,6 @@ class App extends Component<{}, IState> {
           <button className="btn btn-primary Stream-button"
             // when button is click, our react app tries to request
             // new data from the server.
-            // As part of your task, update the getDataFromServer() function
-            // to keep requesting the data every 100ms until the app is closed
-            // or the server does not return anymore data.
             onClick={() => {this.getDataFromServer()}}>
             Start Streaming Data
           </button>
@@ -72,3 +89,4 @@ class App extends Component<{}, IState> {
 }
 
 export default App;
+
