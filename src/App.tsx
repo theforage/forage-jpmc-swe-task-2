9b92e9977a7523edd74f3657537783e732eb2924
rahulholla97 <rahul@de-haze.com>
2024-07-06 18:09:22 +0530

    patch-2

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean,
 }
 
 /**
@@ -22,6 +23,7 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph: false,
     };
   }
 
@@ -29,18 +31,27 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    if (!this.state.showGraph) {
+      return null;
+    }
+    return (<Graph data={this.state.data}/>);
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+    const fetchData = () => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        this.setState({
+          data: [...this.state.data, ...serverResponds],
+          showGraph: true, // Set showGraph to true when data is fetched
+        });
+      });
+    };
+  
+    fetchData(); // Fetch data immediately
+    setInterval(fetchData, 100); // Fetch data every 100ms
   }
 
   /**
